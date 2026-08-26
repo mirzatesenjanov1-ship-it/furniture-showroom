57bfd1f9dc5fe2e53d9603d59a8468115bdaefbe
Сайт 1 <mirzatesenjanov1@gmail.com>
2026-08-26 12:33:45 +0600

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,1 +1,24 @@
-
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        stone: {
+          850: '#211d1a',
+          950: '#0c0a09',
+        },
+        amber: {
+          950: '#241405',
+        }
+      },
+      fontFamily: {
+        serif: ['Georgia', 'Cambria', 'Times New Roman', 'serif'],
+      }
+    },
+  },
+  plugins: [],
+}
