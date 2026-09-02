405b03d4fb2055ff554f1d05e182b5152a6474a1
Сайт 1 <mirzatesenjanov1@gmail.com>
2026-09-02 13:15:15 +0600

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@viteplugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/furniture-showroom/',
   build: {
     outDir: 'dist',
-    sourcemap: false,
-  },
+    sourcemap: false
+  }
 });
