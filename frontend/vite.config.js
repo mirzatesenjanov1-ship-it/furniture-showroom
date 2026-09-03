be4c4ecb1f73061c364dc777d2ce3793b128c323
Сайт 1 <mirzatesenjanov1@gmail.com>
2026-09-03 09:36:08 +0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,19 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/furniture-showroom/',
-  esbuild: {
-    logOverride: { 'this-is-undefined-in-esm': 'silent' },
-    ignoreAnnotations: true
-  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    chunkSizeWarningLimit: 2000,
-    minify: false,
-    rollupOptions: {
-      onwarn(warning, warn) {
-        return; // Бардык warning'дерди толугу менен өчүрүү
-      }
-    }
+    minify: false
   }
 });
